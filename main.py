1d619334994d39128fc4c380faa9073f807ac0d1
Sergio Andres Tiria Lancheros <sergiotila@unisabana.edu.co>
2026-04-28 00:25:54 -0500

    v7

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import sys
 from todo_manager import read_todo_file, write_todo_file
 
 try:
-    
     if len(sys.argv) < 2:
         raise IndexError("Insufficient arguments provided!")
 
-  
     if sys.argv[1] == "--help":
         print("""Usage: python main.py <file_path> <command> [arguments]...
 
@@ -25,28 +23,31 @@ Examples:
 
     file_path = sys.argv[1]
 
-    tasks = read_todo_file(file_path)
-
     i = 2
     modified = False
+    tasks = None  
 
     while i < len(sys.argv):
         command = sys.argv[i]
-
         
         if command == "add":
             if i + 1 >= len(sys.argv):
                 raise IndexError('Task description required for "add".')
+
+            if tasks is None:
+                tasks = read_todo_file(file_path)
 
             task = sys.argv[i + 1]
             tasks.append(task)
             print(f'Task "{task}" added.')
             modified = True
             i += 2
-
         elif command == "remove":
             if i + 1 >= len(sys.argv):
                 raise IndexError('Task description required for "remove".')
+
+            if tasks is None:
+                tasks = read_todo_file(file_path)
 
             task = sys.argv[i + 1]
             try:
@@ -57,13 +58,16 @@ Examples:
                 print(f'Task "{task}" not found.')
             i += 2
 
-        
         elif command == "view":
+            if tasks is None:
+                tasks = read_todo_file(file_path)
+
             if tasks:
                 print("Tasks:")
                 for task in tasks:
                     print(task)
             i += 1
+
         else:
             raise ValueError("Command not found!")
 
